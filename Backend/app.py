9c33a61b70d362ab6318ff0534fdde5d1882718f
Aryan0102 <aryansumit0102@gmail.com>
2025-02-07 23:16:55 -0500

    Initial setup

---
+++ Backend/app.py
@@ -0,0 +1,46 @@
+# Imports
+from flask import Flask
+from flask_cors import CORS
+from config import db, JWT_SECRET_KEY
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+
+#Set up Flask, CORS and Bcrypt
+app = Flask(__name__)
+CORS(app)
+bcrypt = Bcrypt(app)
+
+#Set JWT configuration
+app.config["JWT_SECRET_KEY"] = JWT_SECRET_KEY
+jwt= JWTManager(app)
+
+# Blueprints
+
+
+# End Blueprints
+
+# MESSAGES
+# Reinstall pip packages (activate venv, then run pip install -r requirements.txt)
+# PLEASE GIT PULL, TONS CHANGED
+
+# Home page
+@app.route("/", methods=["GET"])
+def home():
+    return {"message": "Backend is working"}, 200
+
+@app.route('/api/all', methods=["GET"])
+def get_all_routes():
+    routes = []
+    try:
+        for route in app.url_map.iter_rules():
+            routes.append({
+                'Endpoint': route.endpoint,
+                'Accepts': ', '.join(route.methods),
+                'Route': str(route)
+            })
+        return (routes)
+    except Exception as e:
+        return ({"error": str(e), "status":500})
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=9999)
---
+++ Backend/config.py
@@ -0,0 +1,32 @@
+# Imports
+import os
+from pymongo import MongoClient
+import mongoengine as me
+from dotenv import load_dotenv
+from flask_jwt_extended import JWTManager
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get MongoDB URI from environment variables
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/Hackbeanpot")
+
+# JWT Secret Key
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+
+# Create a MongoDB client connection
+try:
+    client = MongoClient(MONGO_URI)
+    db = client.get_database("Hackbeanpot")
+    print("Connected to MongoDB")
+except Exception as e:
+    print(f"Error connecting to MongoDB: {e}")
+    exit(1)
+
+# Connect to MongoEngine using same MongoDB URI
+try:
+    me.connect("Hackbeanpot", host=MONGO_URI)
+    print("Connected MongoDB to MongoEngine")
+except Exception as e:
+    print(f"Error connecting MongoDB to MongoEngine: {e}")
+    exit(1)
